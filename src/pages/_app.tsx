548ca0cec11644c0193e260d6479aaee33224f0c
Marcello Bastea-Forte <marcello@cellosoft.com>
2020-08-08 22:21:41 -0700

    bad imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,4 @@
 import '../styles/globals.css';
-import 'react-virtualized/styles.css';
-
-import { GraphqlWrapper } from '../components/GraphqlWrapper';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
